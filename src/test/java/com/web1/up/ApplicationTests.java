6fa55fd7a311696894e666c102c2e6c19d934b7d
Saransh Tyagi <tyagisaransh90@gmail.com>
2024-07-05 16:44:17 +0530

    setup project structure and add dependiencies

---
+++ src/test/java/com/web1/up/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web1.up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
